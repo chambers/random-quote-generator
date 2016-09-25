25a484bac23a06ecc1c1c8e9e55cf755bd682ba5
Kieran Chambers <kieranchambers@gmail.com>
2016-09-25 21:24:17 +0100

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,326 @@
+var quotes = [{
+    "Season": "1",
+    "Episode": "\"The Target\"",
+    "Epigraph": "…when it’s not your turn.",
+    "Attributed to": "McNulty"
+}, {
+    "Season": "1",
+    "Episode": "\"The Detail\"",
+    "Epigraph": "You cannot lose if you do not play.",
+    "Attributed to": "Marla Daniels"
+}, {
+    "Season": "1",
+    "Episode": "\"The Buys\"",
+    "Epigraph": "The king stay the king.",
+    "Attributed to": "D'Angelo"
+}, {
+    "Season": "1",
+    "Episode": "\"Old Cases\"",
+    "Epigraph": "It's a thin line 'tween heaven and here",
+    "Attributed to": "Bubbles"
+}, {
+    "Season": "1",
+    "Episode": "\"The Pager\"",
+    "Epigraph": "…a little slow, a little late.",
+    "Attributed to": "Avon Barksdale"
+}, {
+    "Season": "1",
+    "Episode": "\"The Wire\"",
+    "Epigraph": "…and all the pieces matter.",
+    "Attributed to": "Freamon"
+}, {
+    "Season": "1",
+    "Episode": "\"Lessons\"",
+    "Epigraph": "A man must have a code.",
+    "Attributed to": "Bunk"
+}, {
+    "Season": "1",
+    "Episode": "\"One Arrest\"",
+    "Epigraph": "Come at the king, you best not miss.",
+    "Attributed to": "Omar"
+}, {
+    "Season": "1",
+    "Episode": "\"Game Day\"",
+    "Epigraph": "Maybe we won.",
+    "Attributed to": "Herc"
+}, {
+    "Season": "1",
+    "Episode": "\"The Cost\"",
+    "Epigraph": "And then he dropped the bracelets…",
+    "Attributed to": "Greggs"
+}, {
+    "Season": "1",
+    "Episode": "\"The Hunt\"",
+    "Epigraph": "Dope on the damn table.",
+    "Attributed to": "Daniels"
+}, {
+    "Season": "1",
+    "Episode": "\"Cleaning Up\"",
+    "Epigraph": "This is me, yo, right here.",
+    "Attributed to": "Wallace"
+}, {
+    "Season": "1",
+    "Episode": "\"Sentencing\"",
+    "Epigraph": "All in the game...",
+    "Attributed to": "Traditional West Baltimore"
+}, {
+    "Season": "2",
+    "Episode": "\"Ebb Tide\"",
+    "Epigraph": "Ain't never gonna be what it was.",
+    "Attributed to": "Little Big Roy"
+}, {
+    "Season": "2",
+    "Episode": "\"Collateral Damage\"",
+    "Epigraph": "They can chew you up, but they gotta spit you out.",
+    "Attributed to": "McNulty"
+}, {
+    "Season": "2",
+    "Episode": "\"Hot Shots\"",
+    "Epigraph": "What they need is a union.",
+    "Attributed to": "Russell"
+}, {
+    "Season": "2",
+    "Episode": "\"Hard Cases\"",
+    "Epigraph": "If I hear music, I’m gonna dance.",
+    "Attributed to": "Greggs"
+}, {
+    "Season": "2",
+    "Episode": "\"Undertow\"",
+    "Epigraph": "They used to make steel there, no?",
+    "Attributed to": "Vondas"
+}, {
+    "Season": "2",
+    "Episode": "\"All Prologue\"",
+    "Epigraph": "It don’t matter that some fool say he different…",
+    "Attributed to": "D'Angelo"
+}, {
+    "Season": "2",
+    "Episode": "\"Backwash\"",
+    "Epigraph": "Don’t worry kid. You’re still on the clock.",
+    "Attributed to": "Horseface"
+}, {
+    "Season": "2",
+    "Episode": "\"Duck and Cover\"",
+    "Epigraph": "How come they don’t fly away?",
+    "Attributed to": "Ziggy"
+}, {
+    "Season": "2",
+    "Episode": "\"Stray Rounds\"",
+    "Epigraph": "The world is a smaller place now.",
+    "Attributed to": "The Greek"
+}, {
+    "Season": "2",
+    "Episode": "\"Storm Warnings\"",
+    "Epigraph": "It pays to go with the union card every time.",
+    "Attributed to": "Ziggy"
+}, {
+    "Season": "2",
+    "Episode": "\"Bad Dreams\"",
+    "Epigraph": "I need to get clean.",
+    "Attributed to": "Sobotka"
+}, {
+    "Season": "2",
+    "Episode": "\"Port in a Storm\"",
+    "Epigraph": "Business. Always business.",
+    "Attributed to": "The Greek"
+}, {
+    "Season": "3",
+    "Episode": "\"Time After Time\"",
+    "Epigraph": "Don't matter how many times you get burnt, you just keep doin' the same.",
+    "Attributed to": "Bodie"
+}, {
+    "Season": "3",
+    "Episode": "\"All Due Respect\"",
+    "Epigraph": "There's never been a paper bag.",
+    "Attributed to": "Colvin"
+}, {
+    "Season": "3",
+    "Episode": "\"Dead Soldiers\"",
+    "Epigraph": "The Gods will not save you.",
+    "Attributed to": "Burrell"
+}, {
+    "Season": "3",
+    "Episode": "\"Amsterdam\"",
+    "Epigraph": "Why you gotta go and fuck with the program?",
+    "Attributed to": "Fruit"
+}, {
+    "Season": "3",
+    "Episode": "\"Straight and True\"",
+    "Epigraph": "I had such fucking hopes for us.",
+    "Attributed to": "McNulty"
+}, {
+    "Season": "3",
+    "Episode": "\"Homecoming\"",
+    "Epigraph": "Just a gangster, I suppose.",
+    "Attributed to": "Avon Barksdale"
+}, {
+    "Season": "3",
+    "Episode": "\"Back Burners\"",
+    "Epigraph": "Conscience do cost.",
+    "Attributed to": "Butchie"
+}, {
+    "Season": "3",
+    "Episode": "\"Moral Midgetry\"",
+    "Epigraph": "Crawl, walk, and then run.",
+    "Attributed to": "Clay Davis"
+}, {
+    "Season": "3",
+    "Episode": "\"Slapstick\"",
+    "Epigraph": "…while you’re waiting for moments that never come.",
+    "Attributed to": "Freamon"
+}, {
+    "Season": "3",
+    "Episode": "\"Reformation\"",
+    "Epigraph": "Call it a crisis of leadership.",
+    "Attributed to": "Proposition Joe"
+}, {
+    "Season": "3",
+    "Episode": "\"Middle Ground\"",
+    "Epigraph": "We ain't gotta dream no more, man.",
+    "Attributed to": "Stringer Bell"
+}, {
+    "Season": "3",
+    "Episode": "\"Mission Accomplished\"",
+    "Epigraph": "We fight on that lie.",
+    "Attributed to": "Slim Charles"
+}, {
+    "Season": "4",
+    "Episode": "\"Boys of Summer\"",
+    "Epigraph": "Lambs to the slaughter here.",
+    "Attributed to": "Marcia Donnelly"
+}, {
+    "Season": "4",
+    "Episode": "\"Soft Eyes\"",
+    "Epigraph": "I still wake up white in a city that ain't.",
+    "Attributed to": "Carcetti"
+}, {
+    "Season": "4",
+    "Episode": "\"Home Rooms\"",
+    "Epigraph": "I love the first day, man. Everybody all friendly an' shit",
+    "Attributed to": "Namond Brice"
+}, {
+    "Season": "4",
+    "Episode": "\"Refugees\"",
+    "Epigraph": "No one wins. One side just loses more slowly.",
+    "Attributed to": "Prez"
+}, {
+    "Season": "4",
+    "Episode": "\"Alliances\"",
+    "Epigraph": "If you with us, you with us.",
+    "Attributed to": "Chris Partlow"
+}, {
+    "Season": "4",
+    "Episode": "\"Margin of Error\"",
+    "Epigraph": "Don't try this shit at home.",
+    "Attributed to": "Norman Wilson"
+}, {
+    "Season": "4",
+    "Episode": "\"Unto Others\"",
+    "Epigraph": "Aw yeah. That golden rule.",
+    "Attributed to": "Bunk"
+}, {
+    "Season": "4",
+    "Episode": "\"Corner Boys\"",
+    "Epigraph": "We got our thing, but it’s just part of the big thing.",
+    "Attributed to": "Zenobia"
+}, {
+    "Season": "4",
+    "Episode": "\"Know Your Place\"",
+    "Epigraph": "Might as well dump 'em, get another.",
+    "Attributed to": "Proposition Joe"
+}, {
+    "Season": "4",
+    "Episode": "\"Misgivings\"",
+    "Epigraph": "World goin' one way, people another.",
+    "Attributed to": "Poot"
+}, {
+    "Season": "4",
+    "Episode": "\"A New Day\"",
+    "Epigraph": "You play in dirt, you get dirty",
+    "Attributed to": "McNulty"
+}, {
+    "Season": "4",
+    "Episode": "\"That's Got His Own\"",
+    "Epigraph": "That all there is to it?",
+    "Attributed to": "Bubbles"
+}, {
+    "Season": "4",
+    "Episode": "\"Final Grades\"",
+    "Epigraph": "If animal trapped call 410-844-6286",
+    "Attributed to": "Baltimore, traditional"
+}, {
+    "Season": "5",
+    "Episode": "\"More with Less\"",
+    "Epigraph": "The bigger the lie, the more they believe.",
+    "Attributed to": "Bunk"
+}, {
+    "Season": "5",
+    "Episode": "\"Unconfirmed Reports\"",
+    "Epigraph": "This ain't Aruba, bitch.",
+    "Attributed to": "Bunk"
+}, {
+    "Season": "5",
+    "Episode": "\"Not for Attribution\"",
+    "Epigraph": "They're dead where it doesn't count.",
+    "Attributed to": "Fletcher"
+}, {
+    "Season": "5",
+    "Episode": "\"Transitions\"",
+    "Epigraph": "Buyer's market out there.",
+    "Attributed to": "Templeton"
+}, {
+    "Season": "5",
+    "Episode": "\"React Quotes\"",
+    "Epigraph": "Just 'cause they’re in the street doesn't mean that they lack opinions.",
+    "Attributed to": "Haynes"
+}, {
+    "Season": "5",
+    "Episode": "\"The Dickensian Aspect\"",
+    "Epigraph": "If you have a problem with this, I understand completely.",
+    "Attributed to": "Freamon"
+}, {
+    "Season": "5",
+    "Episode": "\"Took\"",
+    "Epigraph": "They don't teach it in law school.",
+    "Attributed to": "Pearlman"
+}, {
+    "Season": "5",
+    "Episode": "\"Clarifications\"",
+    "Epigraph": "A lie ain't a side of the story, it's just a lie.",
+    "Attributed to": "Terry Hanning"
+}, {
+    "Season": "5",
+    "Episode": "\"Late Editions\"",
+    "Epigraph": "Deserve got nuthin' to do with it.",
+    "Attributed to": "Snoop"
+}, {
+    "Season": "5",
+    "Episode": "\"–30–\"",
+    "Epigraph": "The Life of Kings",
+    "Attributed to": "H. L. Mencken"
+}];
+
+$(document).ready(function() {
+    function getRandomQuote() {
+        var randomItem = quotes[Math.floor(Math.random() * quotes.length)];
+        var html = "<div class=\"quote\"><p id=\"epigraph\">\"" + randomItem.Epigraph + "\" --" + randomItem["Attributed to"] + "</p><p>Season " + randomItem.Season + ", " + randomItem.Episode + "</p></div>";
+        $(".quote").html(html);
+    }
+    getRandomQuote();
+    $("#getQuote").on("click", function() {
+        getRandomQuote();
+    });
+
+});
+
+$("#tweetThis").on("click", function() {
+    var params = {
+        text: "",
+        related: "The Wire",
+        hashtags: "theWire"
+    };
+    params.text = $("#epigraph").text();
+    var encodedParams = $.param(params);
+    var url = "https://twitter.com/intent/tweet?" + encodedParams;
+    window.open(url, "_blank");
+});
